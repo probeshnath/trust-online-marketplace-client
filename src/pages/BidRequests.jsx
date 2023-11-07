5bc727ba5435df5765f1c8c8ad10adc9fc5e60fd
probeshnath <probeshn@gmail.com>
2023-11-08 01:06:05 +0600

    reactprogress bar added

--- src/pages/BidRequests.jsx
+++ src/pages/BidRequests.jsx
@@ -2,101 +2,110 @@ import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../provider/AuthProvider'
 import axios from 'axios'
 import toast from 'react-hot-toast'
+import "react-step-progress-bar/styles.css";
+import { ProgressBar } from 'react-step-progress-bar'
 
 const BidRequests = () => {
-    const [bidRequests, setBidRequests] = useState()
-    const { user } = useContext(AuthContext)
-    console.log(user.email)
-  
-    useEffect(() => {
-      axios.get(`http://localhost:5000/bidrequests?email=${user.email}`)
-        .then(res => {
-          console.log(res.data)
-          setBidRequests(res.data)
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    }, []);
+  const [bidRequests, setBidRequests] = useState()
+  const { user } = useContext(AuthContext)
+  console.log(user.email)
 
-    // accept job
-    const handleAcceptJobRequest = (id) =>{
-        // console.log("this is id:",id)
-        let job_Status = {job_Status : "Progress"};
-        axios.put(`http://localhost:5000/bid/update/${id}`,job_Status)
-        .then(res =>{
-          console.log(res.data)
-          if(res.data.modifiedCount > 0){
-            toast.success("Accept the Bid")
-          }
-        })
-        .catch(error =>{
-          console.log(error)
-        })
-    }
-
-     // accept job
-     const handleRejectJobRequest = (id) =>{
-       // console.log("this is id:",id)
-       let job_Status = {job_Status : "Canceled"};
-      axios.put(`http://localhost:5000/bid/update/${id}`,job_Status)
-      .then(res =>{
+  useEffect(() => {
+    axios.get(`http://localhost:5000/bidrequests?email=${user.email}`)
+      .then(res => {
         console.log(res.data)
-        if(res.data.modifiedCount > 0){
+        setBidRequests(res.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, []);
+
+  // accept job
+  const handleAcceptJobRequest = (id) => {
+    // console.log("this is id:",id)
+    let job_Status = { job_Status: "Progress" };
+    axios.put(`http://localhost:5000/bid/update/${id}`, job_Status)
+      .then(res => {
+        console.log(res.data)
+        if (res.data.modifiedCount > 0) {
+          toast.success("Accept the Bid")
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  // accept job
+  const handleRejectJobRequest = (id) => {
+    // console.log("this is id:",id)
+    let job_Status = { job_Status: "Canceled" };
+    axios.put(`http://localhost:5000/bid/update/${id}`, job_Status)
+      .then(res => {
+        console.log(res.data)
+        if (res.data.modifiedCount > 0) {
           toast.success("Reject the Bid")
         }
       })
-      .catch(error =>{
+      .catch(error => {
         console.log(error)
       })
-     }
+  }
 
 
   return (
     <div className='max-w-7xl mx-auto py-5'>
-    <h2 className='pt-2 pb-4 text-center text-3xl font-bold text-green-800'>My Bits</h2>
-    <div>
-      <div className="overflow-x-auto">
-        <table className="table table-xs">
-          <thead>
-            <tr>
-              <th>Serial</th>
-              <th>Job Title</th>
-              <th>Seller Email</th>
-              <th>Deadline</th>
-              <th>Price</th>
-              <th>Status</th>
-              <th>Action</th>
-              {/* <th>Favorite Color</th> */}
-            </tr>
-          </thead>
+      <h2 className='pt-2 pb-4 text-center text-3xl font-bold text-green-800'>My Bits</h2>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table table-xs">
+            <thead>
+              <tr>
+                <th>Serial</th>
+                <th>Job Title</th>
+                <th>Seller Email</th>
+                <th>Deadline</th>
+                <th>Price</th>
+                <th>Status</th>
+                <th>Action</th>
+                {/* <th>Favorite Color</th> */}
+              </tr>
+            </thead>
 
-          <tbody>
-            {
-              bidRequests?.map((bid,inx) => (
-                <tr key={inx}>
-                  <th>{inx+1}</th>
-                  <td>{bid.job_title}</td>
-                  <td>{bid.seller_email}</td>
-                  <td>{bid.deadline}</td>
-                  <td>{bid.price}</td>
-                  <td>{bid.job_Status}</td>
-                  <td>
+            <tbody>
+              {
+                bidRequests?.map((bid, inx) => (
+                  <tr key={inx}>
+                    <th>{inx + 1}</th>
+                    <td>{bid.job_title}</td>
+                    <td>{bid.seller_email}</td>
+                    <td>{bid.deadline}</td>
+                    <td>{bid.price}</td>
+                    <td>{bid.job_Status}</td>
 
-                    <button onClick={() =>handleAcceptJobRequest(bid._id)} className='bg-green-600 rounded-md text-white mr-2 py-1 px-2'>Accept</button>
-                   <button onClick={() =>handleRejectJobRequest(bid._id)} className='bg-red-600 rounded-md text-white py-1 px-2'>Reject</button></td>
-                  
-                </tr>
-              ))
-            } 
+                    <td>
+                      <button onClick={() => handleAcceptJobRequest(bid._id)} className='bg-green-600 rounded-md text-white mr-2 py-1 px-2'>Accept</button>
+                      <button onClick={() => handleRejectJobRequest(bid._id)} className='bg-red-600 rounded-md text-white py-1 px-2'>Reject</button>
+                    </td>
+                    <td>
+                      <ProgressBar
+                        width={80}
+                        percent={100}
+                        filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+                      />
+                    </td>
+                  </tr>
+                ))
+              }
 
-          </tbody>
+            </tbody>
 
-        </table>
+          </table>
+        </div>
       </div>
-    </div>
 
-  </div>
+    </div>
   )
 }
 
--- src/pages/MyBits.jsx
+++ src/pages/MyBits.jsx
@@ -1,11 +1,30 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../provider/AuthProvider'
 import axios from 'axios'
+import toast from 'react-hot-toast'
 
 const MyBids = () => {
   const [bids, setBids] = useState()
   const { user } = useContext(AuthContext)
-  console.log(user.email)
+  // console.log(user.email)
+
+   // complete task
+   const handleCompleteJobRequest = (id) => {
+    // console.log("this is id:",id)
+    let job_Status = { job_Status: "Complete" };
+    axios.put(`http://localhost:5000/bid/update/${id}`, job_Status)
+      .then(res => {
+        console.log(res.data)
+        if (res.data.modifiedCount > 0) {
+          toast.success("Complete the Task")
+        }
+      })
+      .catch(error => {
+        console.log(error)
+        toast.error(error)
+      })
+  }
+
 
   useEffect(() => {
     axios.get(`http://localhost:5000/bids?email=${user.email}`)
@@ -16,7 +35,9 @@ const MyBids = () => {
       .catch(error => {
         console.log(error)
       })
-  }, [])
+  }, [handleCompleteJobRequest])
+
+ 
 
   return (
     <div className='max-w-7xl mx-auto py-5'>
@@ -38,14 +59,14 @@ const MyBids = () => {
 
             <tbody>
               {
-                bids?.map((bid,inx) => (
-                  <tr>
-                    <th>{inx+1}</th>
+                bids?.map((bid, inx) => (
+                  <tr key={inx}>
+                    <th>{inx + 1}</th>
                     <td>{bid.job_title}</td>
                     <td>{bid.buyer_email}</td>
                     <td>{bid.deadline}</td>
                     <td>{bid.job_Status}</td>
-                    <td>Compelte</td>
+                    <td><button className={`${(bid.job_Status !== "Pending" && bid.job_Status !== "Complete") ? "bg-green-600 rounded-md py-1 px-2 text-white " : "bg-gray-500 rounded-md py-1 px-2 text-white"}`} disabled={(bid.job_Status === "Pending" || bid.job_Status === "Complete") ? true : false} onClick={() => handleCompleteJobRequest(bid._id)}>Compelte</button></td>
                     {/* <td>Blue</td> */}
                   </tr>
                 ))
