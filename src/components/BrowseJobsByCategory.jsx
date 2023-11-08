e55f5437f4eff602f2a96900c7027e53ced31f90
probeshnath <probeshn@gmail.com>
2023-11-08 15:22:44 +0600

    jobs load by category

--- src/components/BrowseJobsByCategory.jsx
+++ src/components/BrowseJobsByCategory.jsx
@@ -10,27 +10,29 @@ const BrowseJobsByCategory = () => {
     const [category, setCategory] = useState()
 
 
-    console.log("jobs", jobs)
+    // console.log("category", category)
 
-    // useEffect(()=>{
-    //     axios.get(`http://localhost:5000/jobs`,)
-    //     .then(res =>{
-    //         console.log(res.data)
-    //         setJobs(res.data)
-    //     })
-    //     .catch(error =>{
-    //         console.log(error)
-    //     })
-    // },[])
+    useEffect(()=>{
+        // let cate = {category : category}
+        // console.log("category",cate)
+        axios.get(`http://localhost:5000/jobs?category=${category}`,)
+        .then(res =>{
+            console.log("cate",res.data)
+            setJobs(res.data)
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    },[category])
     return (
         <div className='bg-gray-200 py-5'>
 
             <Tabs className='max-w-7xl mx-auto'>
-                <TabList className="flex justify-center py-5 gap-3">
-                    <Tab className="bg-orange-700 py-1 rounded-md px-2 text-white" >All</Tab>
-                    <Tab>Web Development</Tab>
-                    <Tab>Graphics Design</Tab>
-                    <Tab className="border border-orange-600 py-1 px-2 rounded-md cursor-pointer" >Digital Marketing</Tab>
+                <TabList className="flex justify-center items-center py-5 font-bold gap-3">
+                    <Tab onClick={()=> setJobs(allJobs)} className="bg-orange-700 py-1 rounded-md px-2 text-white" >All</Tab>
+                    <Tab onClick={()=> setCategory("Web Development")} className="bg-orange-700 py-1 rounded-md px-2 text-white">Web Development</Tab>
+                    <Tab onClick={()=> setCategory("Graphics Design")} className="bg-orange-700 py-1 rounded-md px-2 text-white">Graphics Design</Tab>
+                    <Tab onClick={()=> setCategory("Digital Marketing")} className="bg-orange-700 py-1 rounded-md px-2 text-white" >Digital Marketing</Tab>
                 </TabList>
 
                 <TabPanel>
@@ -44,72 +46,36 @@ const BrowseJobsByCategory = () => {
                     </div>
                 </TabPanel>
                 <TabPanel>
-                    <p>
-                        <b>Luigi</b> (<i>Japanese: ルイージ Hepburn: Ruīji, [ɾɯ.iː.dʑi̥]</i>) (<i>English: /luˈiːdʒi/;
-                            Italian: [luˈiːdʒi]</i>) is a fictional character featured in video games and related media
-                        released by Nintendo. Created by prominent game designer Shigeru Miyamoto, Luigi is portrayed
-                        as the slightly younger but taller fraternal twin brother of Nintendo's mascot Mario, and
-                        appears in many games throughout the Mario franchise, often as a sidekick to his brother.
-                    </p>
-                    <p>
-                        Source:{' '}
-                        <a href="https://en.wikipedia.org/wiki/Luigi" target="_blank">
-                            Wikipedia
-                        </a>
-                    </p>
+                <h2 className='py-5 text-center text-3xl font-bold text-green-700 '>Web Development <span className='text-orange-600'>Jobs</span></h2>
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-3 px-3'>
+                        {
+                            jobs && jobs.map((job)=>(
+                                <Job key={job._id} job={job} />
+                            ))
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <p>
-                        <b>Princess Peach</b> (<i>Japanese: ピーチ姫 Hepburn: Pīchi-hime, [piː.tɕi̥ çi̥.me]</i>)
-                        is a character in Nintendo's Mario franchise. Originally created by Shigeru Miyamoto,
-                        Peach is the princess of the fictional Mushroom Kingdom, which is constantly under
-                        attack by Bowser. She often plays the damsel in distress role within the series and
-                        is the lead female. She is often portrayed as Mario's love interest and has appeared
-                        in Super Princess Peach, where she is the main playable character.
-                    </p>
-                    <p>
-                        Source:{' '}
-                        <a href="https://en.wikipedia.org/wiki/Princess_Peach" target="_blank">
-                            Wikipedia
-                        </a>
-                    </p>
+                <h2 className='py-5 text-center text-3xl font-bold text-green-700 '>Graphics Design <span className='text-orange-600'>Jobs</span></h2>
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-3 px-3'>
+                        {
+                            jobs && jobs.map((job)=>(
+                                <Job key={job._id} job={job} />
+                            ))
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <p>
-                        <b>Yoshi</b> (<i>ヨッシー Yosshī, [joɕ.ɕiː]</i>) (<i>English: /ˈjoʊʃi/ or /ˈjɒʃi/</i>), once
-                        romanized as Yossy, is a fictional anthropomorphic dinosaur who appears in
-                        video games published by Nintendo. Yoshi debuted in Super Mario World (1990) on the
-                        Super Nintendo Entertainment System as Mario and Luigi's sidekick. Yoshi later starred
-                        in platform and puzzle games, including Super Mario World 2: Yoshi's Island, Yoshi's Story
-                        and Yoshi's Woolly World. Yoshi also appears in many of the Mario spin-off games, including
-                        Mario Party and Mario Kart, various Mario sports games, and Nintendo's crossover fighting
-                        game series Super Smash Bros. Yoshi belongs to the species of the same name, which is
-                        characterized by their variety of colors.
-                    </p>
-                    <p>
-                        Source:{' '}
-                        <a href="https://en.wikipedia.org/wiki/Yoshi" target="_blank">
-                            Wikipedia
-                        </a>
-                    </p>
+                <h2 className='py-5 text-center text-3xl font-bold text-green-700 '>Digital Marketing <span className='text-orange-600'>Jobs</span></h2>
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-3 px-3'>
+                        {
+                            jobs && jobs.map((job)=>(
+                                <Job key={job._id} job={job} />
+                            ))
+                        }
+                    </div>
                 </TabPanel>
-                <TabPanel>
-                    <p>
-                        <b>Toad</b> (<i>Japanese: キノピオ Hepburn: Kinopio</i>) is a fictional character who primarily
-                        appears in Nintendo's Mario franchise. Created by Japanese video game designer Shigeru Miyamoto,
-                        he is portrayed as a citizen of the Mushroom Kingdom and is one of Princess Peach's most loyal
-                        attendants; constantly working on her behalf. He is usually seen as a non-player character (NPC)
-                        who provides assistance to Mario and his friends in most games, but there are times when Toad(s)
-                        takes center stage and appears as a protagonist, as seen in Super Mario Bros. 2, Wario's Woods,
-                        Super Mario 3D World, and Captain Toad: Treasure Tracker.
-                    </p>
-                    <p>
-                        Source:{' '}
-                        <a href="https://en.wikipedia.org/wiki/Toad_(Nintendo)" target="_blank">
-                            Wikipedia
-                        </a>
-                    </p>
-                </TabPanel>
+                
             </Tabs>
 
 
--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../provider/AuthProvider'
 import { Navigate } from 'react-router-dom'
+import { HashLoader } from 'react-spinners'
 
 const PrivateRoute = ({children}) => {
 const {user, loading} = useContext(AuthContext)
 
 if(loading){
-    return <h2>Loading....</h2>
+    return <div className='flex justify-center items-center h-screen'><HashLoader color="#36d7b7" /></div>
 }
 
 if(!user){
